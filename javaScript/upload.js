31b0ea674b74c9853856c0763c568bb9b1551925
Chrisbelefantis <55947209+Chrisbelefantis@users.noreply.github.com>
2021-01-04 15:23:01 +0200

    Finished upload data

--- javaScript/upload.js
+++ javaScript/upload.js
@@ -13,12 +13,12 @@ var fileNames;
 
 
 var loadingSpinner=
-  `<div class="file-status">
+  `
     <span class="status-text">Uploading</span> 
     <div class="spinner-border" role="status">
       <span class="visually-hidden"></span>
     </div>
-  </div>`;
+  `;
 
 var xsvg=`<svg  
   class=\"drop-svg\" 
@@ -31,6 +31,11 @@ var xsvg=`<svg
   d=\"M61.44,0c33.933,0,61.439,27.507,61.439,61.439 s-27.506,61.439-61.439,61.439C27.507,122.879,0,95.372,0,61.439S27.507,0,61.44,0L61.44,0z M73.451,39.151 c2.75-2.793,7.221-2.805,9.986-0.027c2.764,2.776,2.775,7.292,0.027,10.083L71.4,61.445l12.076,12.249 c2.729,2.77,2.689,7.257-0.08,10.022c-2.773,2.765-7.23,2.758-9.955-0.013L61.446,71.54L49.428,83.728 c-2.75,2.793-7.22,2.805-9.986,0.027c-2.763-2.776-2.776-7.293-0.027-10.084L51.48,61.434L39.403,49.185 c-2.728-2.769-2.689-7.256,0.082-10.022c2.772-2.765,7.229-2.758,9.953,0.013l11.997,12.165L73.451,39.151L73.451,39.151z\"/></g>
   </svg>`
 
+var statusPending = 
+`
+  <span class='status-text'> Pending Confirmation </span>
+`
+
 var statusFinished = ` 
 <div  style ='color:green;'>  
   <span class="status-text">Ready</span> 
@@ -42,35 +47,67 @@ var statusFinished = `
 
 //Button for selecting files to upload
 chooseButton.addEventListener('click',function(){
-    
     hiddenChooseButton.click();
 });
 
+
 //Handles when user inputs files
 hiddenChooseButton.addEventListener('change', (event) => {
-  fileNames = Array.from(event.target.files);
-  appendFiles(fileNames);
+  
+  fileNames =  Array.from(event.target.files);
+
+  if(fileNames.length>0){
+    chooseButton.classList.add('hidden');
+    appendFiles(fileNames);
+
+  }
+
 });
 
 
 //Puts filelist to table shows buttons then calls clearFiles()
 function appendFiles(files){
 
-  const filesTable=document.getElementById("filesTable");
+  //Finding the existing filenames in order to mark dublicates
+  existedFileNames = [];
+  const fileRows = document.querySelectorAll(".filename");
+  for(var i=0;i<fileRows.length;i++){
+    existedFileNames.push(fileRows[i].textContent);
+  }
+ 
+
+  
+  const filesTable=document.querySelector("#filesTable");
   uploadButton.disabled = true;
   downloadButton.disabled = true;
 
   for (const file of files) {
 
+    color = '#212529';
+
     uploadedFiles++;
-    const number = uploadedFiles;
-    const fileName = file.name ? file.name : 'NOT SUPPORTED';
+    let number = uploadedFiles;
+    let fileName = file.name;
+
+    if(fileName.split('.')[1]!='har'){
+      fileName = 'NOT SUPPORTED';
+      color = 'red';
+    }
+
+    if(existedFileNames.includes(fileName)){
+      color = 'red';
+    }
+
+
+
     $(`
-      <tr>
+      <tr style='color:`+color+`'>
         <th class="serial-number" scope=\`row\`>` + number + `</th>
-        <td>` + fileName + `</td>
+        <td class="filename">` + fileName + `</td>
         <td>
-          Pending Confirmation
+          <div class='file-status'>
+            `+statusPending+`
+          </div
         </td>
         <td>
           `+xsvg+`
@@ -100,27 +137,40 @@ function appendFiles(files){
 
 
 confirnButton.addEventListener("click",()=>{
-  alert('confirn');
+  readFiles();
+  fileNames = undefined;
+  confirnButton.classList.add('hidden');
+  dropButton.classList.remove('hidden');
+  uploadButton.classList.remove('hidden');
+  downloadButton.classList.remove('hidden');
+  chooseButton.classList.remove('hidden');
 });
 
 function readFiles(){
 
-  listOfFiles=hiddenChooseButton.files;
+  var filesStatus = document.querySelectorAll(".file-status");
+  var fileDropButtons = document.querySelectorAll(".drop-svg");
+  listOfFiles=fileNames;
+
 
   //For multiple files 
   for (var i = 0; i <listOfFiles.length; i++) {          
     (function(file,index) {
- 
+        
+       
+        //Activating Spinner
+        filesStatus[index].innerHTML = loadingSpinner;
+
+        //Hide drop buttons for each file
+        fileDropButtons[index].classList.add('hidden');
+
+        //Initializing FileReader
         var reader = new FileReader();  
 
         //Action to perform when upload is finished
         reader.onload = function(e) {  
           
-          
-          var filesStatus = document.querySelectorAll(".status-text");
-          filesStatus[index].textContent = "Cleaning";
-
-
+      
           clearFile(e.target.result,index);
 
           filesFinished++;
@@ -219,6 +269,7 @@ downloadButton.addEventListener("click",()=>{
 //Drop button function
 dropButton.addEventListener("click", function(){
   
+  chooseButton.classList.remove("hidden");
   chooseButton.classList.add("main-button");
 
   document.getElementById("filesTable").style.visibility = "hidden"; 
@@ -238,6 +289,7 @@ dropButton.addEventListener("click", function(){
 
   cleanedHarFiles = [];
   uploadedFiles=0;
+  filesFinished = 0;
   hiddenChooseButton.value="";
     
 });
@@ -249,45 +301,59 @@ function setDropFileButtons(){
   var dropFileButtons= document.getElementsByClassName("drop-svg")
   let counter = 0;
   for(var button of dropFileButtons){
-    button.addEventListener('click', dropFileSVG.bind(this,counter))
+    button.addEventListener('click', dropFileSVG)
     counter++;
   }
 }
 
+
 //Function that deletes element when drop file (svg) clicked
-function dropFileSVG(index){
+function dropFileSVG(event){
   
-  $(this).closest('tr').remove();
+  //Finding the index of file we want to delete
+  indexInPath = event.path.findIndex((elem)=>elem.localName=='tr');
+  index = event.path[indexInPath].querySelector('th').innerHTML - 1;
+  
+  //Deleting the row of this index
+  tableRows= document.querySelectorAll('#tbody tr');
+  tableRows[index].parentNode.removeChild(tableRows[index]);
+
   uploadedFiles--;
-  filesFinished--;
+ 
   if(uploadedFiles==0){
     dropButton.click();
     return;
   }
+
+  if(uploadedFiles==filesFinished){
+    downloadButton.disabled = false;
+    uploadButton.disabled = false;
+
+  }
+
   const serialNumbers=document.getElementsByClassName("serial-number");
   for(var i=0; i<serialNumbers.length;i++){
     serialNumbers[i].innerHTML=i+1;
   }
   
-  fileNames.splice(index,1)
-  console.log(fileNames);
+  //Deleting the actual file in our list of files to be uploaded
+  fileNames.splice(index-filesFinished,1);
+ 
 
 }
 
 
 
 uploadButton.addEventListener("click",()=>{
-
   
   var userData = findUserData();
 
-  var tempData = {
-    location: '42,12',
-    provider: 'Cosmote'
-  };
+    var tempData = {
+      location: '42,12',
+      provider: 'Cosmote'
+    };
 
-  postData(tempData);
-
+    postData(tempData);
 
 });
 
