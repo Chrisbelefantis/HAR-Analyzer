f4959e066555bd41b3a3115f8ba410e6544f332b
alogd <giwrgos.al.98@gmail.com>
2021-02-12 21:12:41 +0200

    first stage header analysis

--- javaScript/admin/http-headers-analysis.js
+++ javaScript/admin/http-headers-analysis.js
@@ -2,6 +2,94 @@ var content_type=[];
 var average_ttl=[];
 var count=[];
 var provider=[];
+var preferences_button=document.getElementById("pref-button");
+var myPieChart;
+
+function addButtonGroupLogic(optionButtons,allButton){
+    
+    for(let i=0;i<optionButtons.length-1;i++){
+        (function(index){
+                optionButtons[index].addEventListener('click',()=>{
+                    
+                    var allUnselected = false;
+
+                    if(allButton.classList.contains('selected')){
+                        allButton.classList.remove('selected')
+                    }
+                    else{
+
+                        //Checking if all the other buttons except from the 
+                        //one pressed are unselected
+                        allUnselected = true;
+                        for(let j=0;j<optionButtons.length-1;j++){
+                            if(j!==index)
+                            {
+                                allUnselected = allUnselected && !optionButtons[j].classList.contains('selected');
+                            }else{
+                               
+                            }  
+                        }
+
+                    }
+
+                    
+
+                    if(optionButtons[index].classList.contains('selected')){
+                        //To prevent from having a button group without anything selected
+                        if(!allUnselected){
+                            optionButtons[index].classList.remove('selected');
+                        }else{
+                            document.querySelector('.alert').classList.add('show');
+                            setTimeout(()=>{
+                                document.querySelector('.alert').classList.remove('show');
+                            },3000);
+                        }
+                    }
+                    else{
+                        optionButtons[index].classList.add('selected');
+                    }
+
+                  
+
+                   
+                    
+
+                    var allSelected = true;
+                    for(let j=0;j<optionButtons.length-1;j++){
+                        allSelected = allSelected && optionButtons[j].classList.contains('selected');
+                    }
+
+                    if(allSelected){
+                        for(let j=0;j<optionButtons.length-1;j++){
+                        optionButtons[j].classList.remove('selected');
+                        }
+                        allButton.classList.add('selected');
+                    }
+                    find_selected_and_make_chart();
+                })
+            }
+        )(i)
+    }
+
+    allButton.addEventListener('click',()=>{
+        if(!allButton.classList.contains('selected')){
+            allButton.classList.add('selected');
+            for(let i=0; i<optionButtons.length-1; i++){
+                optionButtons[i].classList.remove('selected');
+            }
+
+        }
+        find_selected_and_make_chart();
+    });
+
+}
+
+preferences_button.addEventListener("click",()=>{
+    
+    $('#preferencesModal').appendTo("body").modal('show');
+})
+
+
 
 $.get("../includes/admin/get_ttls.php", function(data) {
 
@@ -19,39 +107,8 @@ $.get("../includes/admin/get_ttls.php", function(data) {
     });
 
     
-    chart_maker(["all"],["all"]);
-
-});
-
-
-
-
-function chart_maker(selected_content_types, selected_providers){
-    var cad_content_type=[];
-    var cad_average_ttl=[];
-    var cad_count=[];
-    var cad_provider=[];
-    //cut and dry data of interest
-    if(selected_content_types[0]=="all" && selected_providers[0]== "all"){
-        cad_content_type=content_type;
-        cad_average_ttl=average_ttl;
-        cad_count=count;
-        cad_provider=provider;
-    }else{
-
-        for(var i=0; i<content_type.length; i++){
-            if(selected_content_types.contains(content_type[i]) &&selected_providers.contains(provider[i])){
-                cad_content_type.push(content_type[i]);
-                cad_average_ttl.push(average_ttl[i]);
-                cad_count.push(count[i]);
-                cad_provider.push(provider[i]);
-            }
-        }
-    }
-
-
-    var min_ttl=Number(cad_average_ttl[cad_average_ttl.length-1]);
-    var max_ttl=Number(cad_average_ttl[0]);
+    var min_ttl=Number(average_ttl[average_ttl.length-1]);
+    var max_ttl=Number(average_ttl[0]);
     var ttl_d=(max_ttl-min_ttl)/9;
     var x_axis=[];
     var y_axis=[0,0,0,0,0,0,0,0,0,0];
@@ -60,14 +117,14 @@ function chart_maker(selected_content_types, selected_providers){
 
     }
 
-    for(var i=0; i<cad_count.length; i++){
-        y_axis[parseInt((cad_average_ttl[i]-min_ttl)/ttl_d)]+=cad_count[i];
+    for(var i=0; i<count.length; i++){
+        y_axis[parseInt((average_ttl[i]-min_ttl)/ttl_d)]+=count[i];
     }
    
 
     var ctx = document.getElementById('myTTLChart').getContext('2d');
 
-        var myPieChart = new Chart(ctx, {
+         myPieChart = new Chart(ctx, {
             type: 'bar',
             data: {
                 labels: x_axis,
@@ -87,5 +144,113 @@ function chart_maker(selected_content_types, selected_providers){
                 }
             }
         }); 
+
+});
+
+
+function chart_maker(selected_content_types, selected_providers){
+    console.log(content_type)
+    console.log(average_ttl)
+    console.log(provider)
+    console.log(count);
+    var cad_content_type=[];
+    var cad_average_ttl=[];
+    var cad_count=[];
+    var cad_provider=[];
+    //cut and dry data of interest
+    if(selected_content_types[0]=="all" && selected_providers[0]== "all"){
+        cad_content_type=content_type;
+        cad_average_ttl=average_ttl;
+        cad_count=count;
+        cad_provider=provider;
+
+    }else if(selected_providers[0]=="all"){
+        for(var i=0; i<content_type.length; i++){
+           
+           
+            if(selected_content_types.includes(content_type[i])){
+               
+                cad_content_type.push(content_type[i]);
+                cad_average_ttl.push(average_ttl[i]);
+                cad_count.push(count[i]);
+                cad_provider.push(provider[i]);
+            }
+        }
+
+    }else if(selected_content_types[0]=="all"){
+        for(var i=0; i<content_type.length; i++){
+            if(selected_providers.includes(provider[i])){
+                cad_content_type.push(content_type[i]);
+                cad_average_ttl.push(average_ttl[i]);
+                cad_count.push(count[i]);
+                cad_provider.push(provider[i]);
+            }
+        }
+
+    }else{
+
+        for(var i=0; i<content_type.length; i++){
+            if(selected_content_types.includes(content_type[i]) && selected_providers.includes(provider[i])){
+                cad_content_type.push(content_type[i]);
+                cad_average_ttl.push(average_ttl[i]);
+                cad_count.push(count[i]);
+                cad_provider.push(provider[i]);
+            }
+        }
+    }
+
+    console.log(cad_average_ttl);
+    console.log(cad_count);
+    var min_ttl=Number(cad_average_ttl[cad_average_ttl.length-1]);
+    var max_ttl=Number(cad_average_ttl[0]);
+    var ttl_d=(max_ttl-min_ttl)/9;
+    var x_axis=[];
+    var y_axis=[0,0,0,0,0,0,0,0,0,0];
+    for(var i=0; i<10; i++){
+        x_axis.push(min_ttl+i*ttl_d);
+    }
+
+    for(var i=0; i<cad_count.length; i++){
+        y_axis[parseInt((cad_average_ttl[i]-min_ttl)/ttl_d)]+=cad_count[i];
+    }
+    
+    var ctx = document.getElementById('myTTLChart').getContext('2d');
+    myPieChart.data.datasets[0].data = y_axis;
+    console.log(y_axis);
+    myPieChart.data.labels = x_axis;
+    myPieChart.update();
 }
 
+
+
+var contentTypeButtons = document.querySelectorAll('.content-types button');
+var allContentTypesButton = document.querySelector('.content-types .all-button');
+
+
+var providerTypeButtons = document.querySelectorAll('.providers button');
+var allProviderTypesButton = document.querySelector('.providers .all-button');
+
+
+addButtonGroupLogic(contentTypeButtons,allContentTypesButton);
+addButtonGroupLogic(providerTypeButtons,allProviderTypesButton);
+
+
+
+function find_selected_and_make_chart(){
+
+    var selected_content_types=[];
+    var selected_providers=[];
+    for(var i=0; i<contentTypeButtons.length; i++){
+        if (contentTypeButtons[i].classList.contains('selected')){
+            selected_content_types.push(contentTypeButtons[i].textContent.toLowerCase().trim());
+        }   
+    }
+
+    for(var i=0; i<providerTypeButtons.length; i++){
+        if (providerTypeButtons[i].classList.contains('selected')){
+            selected_providers.push(providerTypeButtons[i].textContent.toLowerCase().trim());
+        }   
+    }
+
+    chart_maker(selected_content_types, selected_providers);
+}
--- views/admin/headers-analysis.php
+++ views/admin/headers-analysis.php
@@ -14,6 +14,8 @@
  if($_SESSION['isAdmin']==0){
    header("Location: ../accessDenied");
  }
+ 
+ include "includes/admin/waiting_times_params.php";
 
 ?>
 
@@ -33,7 +35,68 @@
         </div>
     </div>
 
+    <svg class="pref-button" id="pref-button" width="90" height="90" viewBox="0 0 24 24"xmlns="http://www.w3.org/2000/svg">
+      <path id="svg_11" d="m22.69815,23.00616l-11.36345,-0.04928l-7.67967,-3.36756l-2.71047,-7.84394l1.3963,-4.88706l3.12115,-3.61396l3.94251,-1.60164l4.88706,-0.41068l3.73717,1.35524l3.28542,3.44969l1.64271,5.54415c-0.08624,3.80835 -0.17248,7.6167 -0.25873,11.42505z" stroke-opacity="null" stroke-width="null" stroke="null" fill="#ffffff"/>
+      <path id="svg_1" d="m12,0a12,12 0 0 0 0,24l12,0l0,-12a12,12 0 0 0 -12,-12zm10,22l-10,0a10,10 0 1 1 10,-10l0,10z" fill="#0b032d"/>
+      <path id="svg_3" d="m5.90095,11.95374c-1.313,-1.44646 -1.57692,-3.42038 -0.86265,-5.08043l2.63595,2.94532l2.5984,-0.48406l0.86098,-2.39831l-2.62929,-2.93848c1.80858,-0.43598 3.80108,0.09036 5.1083,1.5311c1.37862,1.51889 1.60423,3.62225 0.75094,5.32877l6.87085,7.57071c0.73109,0.8049 0.64203,2.02569 -0.19801,2.72593c-0.84004,0.69975 -2.11386,0.61517 -2.8447,-0.18973l-6.8628,-7.56324c-1.88912,0.58343 -4.04297,0.07822 -5.42797,-1.44757l0,0z" fill-opacity="null" stroke-opacity="null" stroke-width="null" stroke="null" fill="#0b032d"/>
+  </svg>
 </div>
+
+<div class="modal fade" id="preferencesModal">
+				<div class="modal-dialog modal-dialog-centered ">
+					<div class="modal-content">
+						<div class="modal-header text-center">
+              <h2 >Change Chart's Parameters</h2>
+              
+						</div>
+
+              <div class='controls-container'>
+
+
+                <div class="param-choice content-types text-center">
+                  <!-- <h4>Content Type</h4> -->
+                  <div class='my-btn-group'>
+                    <?php
+                      for($i=0 ; $i<count($request_content_types); $i++){
+                        echo "<button type=\"button\" class=\"btn\">
+                        $request_content_types[$i]
+                        </button>";
+                      }
+                    ?>
+                    <button type="button" class="btn selected all-button">ALL</button>
+                  </div>
+                </div>
+
+
+                <div class="param-choice providers text-center">
+                  <!-- <h4>Provider</h4> -->
+                  <div class='my-btn-group'>
+                    <?php
+                      for($i=0 ; $i<count($request_providers); $i++){
+                        echo "<button type=\"button\" class=\"btn\">
+                        $request_providers[$i]
+                        </button>";
+                      }
+                    ?>
+                    <button type="button" class="btn selected all-button">ALL</button>
+                  </div>
+                </div>
+
+                </div>
+                <button type="button" id="close" data-dismiss="modal" aria-hidden="true">close</button>
+              </div>
+
+              <div class="alert alert-danger alert-dismissible fade" role="alert">
+                At least one of the options must be selected
+              </div>
+             
+          </div> 
+          
+        </div>
+        
+      </div>
+</div>
+                      
 
 <script src="../javascript/admin/http-headers-analysis.js"></script>
 <?php include 'templates/admin/footer.php'?>
